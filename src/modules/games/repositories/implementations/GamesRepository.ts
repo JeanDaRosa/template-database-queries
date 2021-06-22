4f088d399504cc9e815b14227af5ef3c9bffff56
JeanDaRosa <unrealsaifer@gmail.com>
2021-06-21 21:46:51 -0300

    First commit

--- src/modules/games/repositories/implementations/GamesRepository.ts
+++ src/modules/games/repositories/implementations/GamesRepository.ts
@@ -1,4 +1,5 @@
-import { getRepository, Repository } from 'typeorm';
+import { count } from 'node:console';
+import { getManager, getRepository, Repository } from 'typeorm';
 
 import { User } from '../../../users/entities/User';
 import { Game } from '../../entities/Game';
@@ -14,17 +15,25 @@ export class GamesRepository implements IGamesRepository {
 
   async findByTitleContaining(param: string): Promise<Game[]> {
     return this.repository
-      .createQueryBuilder()
+      .createQueryBuilder('games')
+      .where('LOWER (games.title) LIKE LOWER(:title)', { title: `%${param}%` })
+      .getMany()
       // Complete usando query builder
   }
 
   async countAllGames(): Promise<[{ count: string }]> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository
+    .query('SELECT COUNT (title) FROM games')
+
+    ; // Complete usando raw query
   }
 
   async findUsersByGameId(id: string): Promise<User[]> {
-    return this.repository
-      .createQueryBuilder()
+    return await this.repository
+      .createQueryBuilder('games')
+      .relation(Game, 'users')
+      .of(id)
+      .loadMany();
       // Complete usando query builder
   }
 }
